b08b8607a55ff551d23407c24ef7d18b4e2bb5f4
josedaniel187 <jdaldonado555@gmail.com>
2021-06-18 13:01:46 -0400

    añadiendo Css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of  heroes sivingal';
+  title = 'Tour of Heroes';
 }

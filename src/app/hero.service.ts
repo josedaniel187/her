2bfab2036f49db336736d4626cd6ec5a4626957b
josedaniel187 <jdaldonado555@gmail.com>
2021-06-20 12:31:42 -0400

    Agregar navegación con enrutamiento

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,9 +6,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
@@ -17,5 +15,13 @@ export class HeroService {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // Por ahora, suponga que siempre existe un héroe con el "id" especificado.
+    // El manejo de errores se agregará en el siguiente paso del tutorial gg. 
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
